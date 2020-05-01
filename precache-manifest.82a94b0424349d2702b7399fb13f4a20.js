727b656792028f82370d49b54826582419b9f10f
FloTheFlower <florentina.hofbauer@gmail.com>
2020-05-01 16:21:30 -0700

    Updates

---
+++ precache-manifest.82a94b0424349d2702b7399fb13f4a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f08536772ce9f1d35317ee241b0b2d8",
+    "url": "/RockPaperScissors/index.html"
+  },
+  {
+    "revision": "7da36a7b819f26646137",
+    "url": "/RockPaperScissors/static/css/main.b5976cc7.chunk.css"
+  },
+  {
+    "revision": "f418620589925d4ae8b9",
+    "url": "/RockPaperScissors/static/js/2.17c523cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RockPaperScissors/static/js/2.17c523cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da36a7b819f26646137",
+    "url": "/RockPaperScissors/static/js/main.acc67486.chunk.js"
+  },
+  {
+    "revision": "8e3ba3d3e3aa1ad7683e",
+    "url": "/RockPaperScissors/static/js/runtime-main.4241bacb.js"
+  },
+  {
+    "revision": "86a2b44010618506a1b237cef732b77e",
+    "url": "/RockPaperScissors/static/media/logo.86a2b440.jpg"
+  }
+]);
